d08fff77472c51c773bdea1244a4cd18d98137bb
TimurIsroilov123 <timurisroilov@gmail.com>
2020-11-08 00:46:31 +0500

    my useful message

--- app/src/main/java/com/bignerdranch/android/androidacademy/MainActivity.kt
+++ app/src/main/java/com/bignerdranch/android/androidacademy/MainActivity.kt
@@ -1,11 +1,13 @@
 package com.bignerdranch.android.androidacademy
 
+import android.content.Intent
 import android.os.Bundle
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import kotlinx.android.synthetic.main.content_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -17,6 +19,16 @@ class MainActivity : AppCompatActivity() {
         findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                     .setAction("Action", null).show()
+        }
+
+        buttonInt.setOnClickListener {
+            val intent = Intent(this, practice_one::class.java)
+            startActivity(intent)
+        }
+
+        buttonInt2.setOnClickListener {
+            val intent = Intent(this, practice_two::class.java)
+            startActivity(intent)
         }
     }
 
